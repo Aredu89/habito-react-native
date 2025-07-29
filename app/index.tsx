b9ee03f099e564ff0bebc821b334597ca49f8958
Aredu89 <aredu_r89@hotmail.com>
2025-07-29 17:35:06 -0500

    Adjust layout

--- app/index.tsx
+++ app/index.tsx
@@ -12,26 +12,25 @@ export default function HabitListScreen() {
   return (
     <View className="flex-1 p-4 bg-white dark:bg-black">
       <Text className="text-2xl font-bold text-center mb-4 text-blue-600">Your Habits</Text>
+      <View className="flex-row mb-2 items-center justify-between gap-2">
+        <TextInput
+          placeholder="New habit name"
+          className="flex-1 px-3 py-2 border rounded-md text-black dark:text-white bg-white dark:bg-gray-900"
+          value={name}
+          onChangeText={setName}
+        />
+        <Button title="Add" onPress={() => {
+          if (name.trim()) {
+            habitList.send({ type: 'ADD_HABIT', name: name.trim() });
+            setName('');
+          }
+        }} />
+      </View>
 
       <ScrollView contentContainerStyle={{ paddingBottom: 32 }}>
         {habits.map((habit, index) => (
           <HabitCard key={index} habit={habit} index={index} habitList={habitList} />
         ))}
-
-        <View className="flex-row mt-6 items-center justify-between gap-2">
-          <TextInput
-            placeholder="New habit name"
-            className="flex-1 px-3 py-2 border rounded-md text-black dark:text-white bg-white dark:bg-gray-900"
-            value={name}
-            onChangeText={setName}
-          />
-          <Button title="Add" onPress={() => {
-            if (name.trim()) {
-              habitList.send({ type: 'ADD_HABIT', name: name.trim() });
-              setName('');
-            }
-          }} />
-        </View>
       </ScrollView>
     </View>
   );
